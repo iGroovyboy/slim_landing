620f65ca3484a87a01fec950766b2c09be2b52cb
Anton <i.am.groovyboy@gmail.com>
2021-03-09 14:45:16 +0200

    node basic

--- app/Controllers/AboutController.php
+++ app/Controllers/AboutController.php
@@ -12,7 +12,20 @@ class AboutController extends BaseController
     public function default(): string
     {
         $someVar = Config::get('someVar');
+
         $someVar2 = Option::get('someVar');
+        $data = DB::query("SELECT * ...");
+
+        Node::of('Page', 3)->getAll('element');
+        Node::of('Page', 3)->get('element', 'li');
+        Node::of('Page', 12)->get('element', '123');
+
+        Node::of('Page', 12)->update('element', 'hero', 'value');
+
+        Node::of('Page', 12)->deleteAll('element');
+        Node::of('Page', 12)->delete('element', 'header');
+        Node::of('Page', 12)->delete('element', ['footer', 'body']);
+
 
         return View::render('about.html', ['some' => $someVar]);
     }
